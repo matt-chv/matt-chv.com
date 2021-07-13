df85d0c86d5286292e9103e840ba4433978c09bf
matt-chv <contact@matthieuchevrier.com>
2021-07-13 06:13:08 +0000

    adding deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+cp -r ./blog-posts/ /var/www/mattchv/blog-posts/
+cp -r ./css/ /var/www/mattchv/css/
+cp -r ./cubeportfolio/ /var/www/mattchv/cubeportfolio/
+cp -r ./font/ /var/www/mattchv/font/
+cp -r ./icon-fonts/ /var/www/mattchv/icon-fonts/
+cp -r ./images/ /var/www/mattchv/images/
+cp -r ./js/ /var/www/mattchv/js/
+cp -r ./portfolio-detail/ /var/www/mattchv/portfolio-detail/
+cp -r favicon.ico /var/www/mattchv/favicon.ico
+cp index.html /var/www/mattchv/index.html
